a967fc60f0d893563e1eb8c6e189757f5c5c6e5b
Eric Willoughby <willouge@mskcc.org>
2022-12-11 17:03:44 -0500

    Data Catalog fields and facets added

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -33,7 +33,7 @@ class CatalogController < ApplicationController
     #config.per_page = [10,20,50,100]
 
     # solr field configuration for search results/index views
-    config.index.title_field = 'title_tsim'
+    config.index.title_field = 'dataset_title'
     #config.index.display_type_field = 'format'
     #config.index.thumbnail_field = 'thumbnail_path_ss'
 
@@ -55,6 +55,7 @@ class CatalogController < ApplicationController
     #config.show.title_field = 'title_tsim'
     #config.show.display_type_field = 'format'
     #config.show.thumbnail_field = 'thumbnail_path_ss'
+    config.show.title_field = 'dataset_title'
 
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
@@ -80,21 +81,31 @@ class CatalogController < ApplicationController
     #  (useful when user clicks "more" on a large facet and wants to navigate alphabetically across a large set of results)
     # :index_range can be an array or range of prefixes that will be used to create the navigation (note: It is case sensitive when searching values)
 
-    config.add_facet_field 'format', label: 'Format'
-    config.add_facet_field 'pub_date_ssim', label: 'Publication Year', single: true
-    config.add_facet_field 'subject_ssim', label: 'Topic', limit: 20, index_range: 'A'..'Z'
-    config.add_facet_field 'language_ssim', label: 'Language', limit: true
-    config.add_facet_field 'lc_1letter_ssim', label: 'Call Number'
-    config.add_facet_field 'subject_geo_ssim', label: 'Region'
-    config.add_facet_field 'subject_era_ssim', label: 'Era'
+    # config.add_facet_field 'format', label: 'Format'
+    # config.add_facet_field 'pub_date_ssim', label: 'Publication Year', single: true
+    # config.add_facet_field 'subject_ssim', label: 'Topic', limit: 20, index_range: 'A'..'Z'
+    # config.add_facet_field 'language_ssim', label: 'Language', limit: true
+    # config.add_facet_field 'lc_1letter_ssim', label: 'Call Number'
+    # config.add_facet_field 'subject_geo_ssim', label: 'Region'
+    # config.add_facet_field 'subject_era_ssim', label: 'Era'
 
-    config.add_facet_field 'example_pivot_field', label: 'Pivot Field', pivot: ['format', 'language_ssim'], collapsing: true
+    # config.add_facet_field 'example_pivot_field', label: 'Pivot Field', pivot: ['format', 'language_ssim'], collapsing: true
 
-    config.add_facet_field 'example_query_facet_field', label: 'Publish Date', :query => {
-       :years_5 => { label: 'within 5 Years', fq: "pub_date_ssim:[#{Time.zone.now.year - 5 } TO *]" },
-       :years_10 => { label: 'within 10 Years', fq: "pub_date_ssim:[#{Time.zone.now.year - 10 } TO *]" },
-       :years_25 => { label: 'within 25 Years', fq: "pub_date_ssim:[#{Time.zone.now.year - 25 } TO *]" }
-    }
+    # config.add_facet_field 'example_query_facet_field', label: 'Publish Date', :query => {
+    #    :years_5 => { label: 'within 5 Years', fq: "pub_date_ssim:[#{Time.zone.now.year - 5 } TO *]" },
+    #    :years_10 => { label: 'within 10 Years', fq: "pub_date_ssim:[#{Time.zone.now.year - 10 } TO *]" },
+    #    :years_25 => { label: 'within 25 Years', fq: "pub_date_ssim:[#{Time.zone.now.year - 25 } TO *]" }
+    #}
+      #Facets
+      #config.add_facet_field 'origin_fq', label: 'Origin'
+      config.add_facet_field 'subject_domain_fq', label: 'Subject', limit: 5, index_range: 'A'..'Z', collapse: false
+      config.add_facet_field 'onco_trees_fq', label: 'OncoTree Cancer Type', limit: 5, collapse: false
+      config.add_facet_field 'data_locations', label: 'Repository', limit: 5, collapse: false
+      config.add_facet_field 'collection_standards', label: 'Data Tools', limit: 5, collapse: false
+      config.add_facet_field 'related_software', label: 'Analytical/Collection Software', limit: 5, collapse: false
+      config.add_facet_field 'authors', label: 'Author', limit: 5, collapse: false
+      config.add_facet_field 'publishers', label: 'Publisher', limit: 5, collapse: false
+      config.add_facet_field 'subject_of_study', label: 'Subject of Study', limit: 5, collapse: false
 
 
     # Have BL send all facet field names to Solr, which has been the default
@@ -104,32 +115,44 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display
-    config.add_index_field 'title_tsim', label: 'Title'
-    config.add_index_field 'title_vern_ssim', label: 'Title'
-    config.add_index_field 'author_tsim', label: 'Author'
-    config.add_index_field 'author_vern_ssim', label: 'Author'
-    config.add_index_field 'format', label: 'Format'
-    config.add_index_field 'language_ssim', label: 'Language'
-    config.add_index_field 'published_ssim', label: 'Published'
-    config.add_index_field 'published_vern_ssim', label: 'Published'
-    config.add_index_field 'lc_callnum_ssim', label: 'Call number'
+    # config.add_index_field 'title_tsim', label: 'Title'
+    # config.add_index_field 'title_vern_ssim', label: 'Title'
+    # config.add_index_field 'author_tsim', label: 'Author'
+    # config.add_index_field 'author_vern_ssim', label: 'Author'
+    # config.add_index_field 'format', label: 'Format'
+    # config.add_index_field 'language_ssim', label: 'Language'
+    # config.add_index_field 'published_ssim', label: 'Published'
+    # config.add_index_field 'published_vern_ssim', label: 'Published'
+    # config.add_index_field 'lc_callnum_ssim', label: 'Call number'
+    config.add_index_field 'authors', label: 'Author(s)'
+    config.add_index_field 'description', label: 'Description'
+    config.add_index_field 'subject_domain', label: 'Subject'
+    # config.add_index_field 'access_restrictions', label: 'Access Rights'
+    # config.add_index_field 'local_experts', label: 'local_experts'
+    # config.add_index_field 'subject_geographic_area', label: 'subject_geographic_area'
+    # config.add_index_field 'dataset_start_date', label: 'dataset_start_date'
+    # config.add_index_field 'dataset_end_date', label: 'dataset_end_date'
+
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
-    config.add_show_field 'title_tsim', label: 'Title'
-    config.add_show_field 'title_vern_ssim', label: 'Title'
-    config.add_show_field 'subtitle_tsim', label: 'Subtitle'
-    config.add_show_field 'subtitle_vern_ssim', label: 'Subtitle'
-    config.add_show_field 'author_tsim', label: 'Author'
-    config.add_show_field 'author_vern_ssim', label: 'Author'
-    config.add_show_field 'format', label: 'Format'
-    config.add_show_field 'url_fulltext_ssim', label: 'URL'
-    config.add_show_field 'url_suppl_ssim', label: 'More Information'
-    config.add_show_field 'language_ssim', label: 'Language'
-    config.add_show_field 'published_ssim', label: 'Published'
-    config.add_show_field 'published_vern_ssim', label: 'Published'
-    config.add_show_field 'lc_callnum_ssim', label: 'Call number'
-    config.add_show_field 'isbn_ssim', label: 'ISBN'
+    # config.add_show_field 'title_tsim', label: 'Title'
+    # config.add_show_field 'title_vern_ssim', label: 'Title'
+    # config.add_show_field 'subtitle_tsim', label: 'Subtitle'
+    # config.add_show_field 'subtitle_vern_ssim', label: 'Subtitle'
+    # config.add_show_field 'author_tsim', label: 'Author'
+    # config.add_show_field 'author_vern_ssim', label: 'Author'
+    # config.add_show_field 'format', label: 'Format'
+    # config.add_show_field 'url_fulltext_ssim', label: 'URL'
+    # config.add_show_field 'url_suppl_ssim', label: 'More Information'
+    # config.add_show_field 'language_ssim', label: 'Language'
+    # config.add_show_field 'published_ssim', label: 'Published'
+    # config.add_show_field 'published_vern_ssim', label: 'Published'
+    # config.add_show_field 'lc_callnum_ssim', label: 'Call number'
+    # config.add_show_field 'isbn_ssim', label: 'ISBN'
+
+    #config.add_show_field 'dataset_title', label: 'Dataset Title'
+    config.add_show_field 'subject_domain', label: 'Subject'
 
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
@@ -149,7 +172,7 @@ class CatalogController < ApplicationController
     # solr request handler? The one set in config[:default_solr_parameters][:qt],
     # since we aren't specifying it otherwise.
 
-    config.add_search_field 'all_fields', label: 'All Fields'
+    config.add_search_field 'search', label: 'All Fields'
 
 
     # Now we see how to over-ride Solr request handler defaults, in this
@@ -159,7 +182,7 @@ class CatalogController < ApplicationController
     config.add_search_field('title') do |field|
       # solr_parameters hash are sent to Solr as ordinary url query params.
       field.solr_parameters = {
-        'spellcheck.dictionary': 'title',
+        'spellcheck.dictionary': 'dataset_title',
         qf: '${title_qf}',
         pf: '${title_pf}'
       }
@@ -190,10 +213,11 @@ class CatalogController < ApplicationController
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case). Add the sort: option to configure a
     # custom Blacklight url parameter value separate from the Solr sort fields.
-    config.add_sort_field 'relevance', sort: 'score desc, pub_date_si desc, title_si asc', label: 'relevance'
-    config.add_sort_field 'year-desc', sort: 'pub_date_si desc, title_si asc', label: 'year'
-    config.add_sort_field 'author', sort: 'author_si asc, title_si asc', label: 'author'
-    config.add_sort_field 'title_si asc, pub_date_si desc', label: 'title'
+    # config.add_sort_field 'relevance', sort: 'score desc, pub_date_si desc, title_si asc', label: 'relevance'
+    # config.add_sort_field 'year-desc', sort: 'pub_date_si desc, title_si asc', label: 'year'
+    # config.add_sort_field 'author', sort: 'author_si asc, title_si asc', label: 'author'
+    # config.add_sort_field 'title_si asc, pub_date_si desc', label: 'title'
+    config.add_sort_field 'relevance', sort: 'score desc, date_added desc, dataset_title asc', label: 'relevance'
 
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
